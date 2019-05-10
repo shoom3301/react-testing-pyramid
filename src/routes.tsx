2ee628f560512e6df0227429d6f286336b76c801
a.e.kazachenko <a.e.kazachenko@tinkoff.ru>
2019-05-10 16:05:01 +0300

    Move routes to external component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router';
 import { RouteX } from './components/route-x/route-x';
 import { RouteY } from './components/route-y/route-y';
 
-export const Routes = (): ReactNode => (
+export const Routes = (): JSX.Element => (
     <Switch>
         <Route exact path="/x" component={RouteX}/>
         <Route exact path="/y" component={RouteY}/>
